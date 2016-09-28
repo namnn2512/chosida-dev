b9fa23a1ba0ef0c1222fc47c1e5baca4593cd706
nam nguyen <nam.k15cmu@gmail.com>
2016-09-28 09:30:34 +0700

    Update production.js

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,6 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            'mongodb://localhost/chosida'
+            'mongodb://chosidauser:Abcde12345-@ds035766.mlab.com:35766/chosidadb'
   }
 };
