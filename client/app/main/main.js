79074c55f421a0c0c67bcb4e70b0c21d6b763a93
nam nguyen <nam.k15cmu@gmail.com>
2016-09-28 23:22:15 +0700

    Add files via upload

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -6,6 +6,7 @@ angular.module('chosidaApp')
       .state('main', {
         url: '/',
         templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        controllerAs: 'main'
       });
   });
