595d6cccc68637fcfb44459dc2f68c27b6db757f
nam nguyen <nam.k15cmu@gmail.com>
2016-09-29 10:20:03 +0700

    Update navbar.controller.js

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,24 +1,26 @@
-angular.module('chosidaApp', [])
-.controller('NavbarComponent', function($scope, Auth) {
-  $scope.menu = [{
-    'title': 'Home',
-    'state': 'main'
-  },
-  {
-    'title': 'About',
-    'state': 'contact'
-  },
-  {
-    'title': 'Service',
-    'state': 'service'
-  }];
-  $scope.isLoggedIn =  Auth.isLoggedInSync;
-  $scope.isAdmin= Auth.isAdminSync;
-  $scope.getCurrentUser= Auth.getCurrentUserSync;
-  $scope.isCollapsed = true;
-})
-.directive('navbar', function() {
-  return {
-    templateUrl: './navbar.html'
-  };
+'use strict';
+function NavbarComponent(Auth){
+    var self = this;
+    self.menu = [{
+        'title': 'Home',
+        'state': 'main'
+    },
+    {
+        'title': 'About',
+        'state': 'contact'
+    },
+    {
+        'title': 'Service',
+        'state': 'service'
+    }];
+    self.isLoggedIn =  Auth.isLoggedInSync;
+    self.isAdmin= Auth.isAdminSync;
+    self.getCurrentUser= Auth.getCurrentUserSync;
+    self.isCollapsed = true;
+};
+
+angular.module('chosidaApp')
+.component('navbar', {
+  templateUrl: './navbar.html',
+  controller: NavbarComponent
 });
