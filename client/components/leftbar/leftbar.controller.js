b2811993430843f4aa72af030380cd0dcff8e276
nam nguyen <nam.k15cmu@gmail.com>
2016-10-01 14:23:51 +0700

    Update leftbar.controller.js

--- client/components/leftbar/leftbar.controller.js
+++ client/components/leftbar/leftbar.controller.js
@@ -3,8 +3,12 @@
 angular.module('chosidaApp')
   .controller('LeftbarController',function ($http){
     var self = this;
-     $http.get('../components/utils/menu.json').then(function(response) {
+    $http.get('components/utils/menu.json').then(function(response) {
       self.menu = response.data;
+      console.log(self.menu);
+    },
+    function(response){
+        //error callback
     });
 });
 
