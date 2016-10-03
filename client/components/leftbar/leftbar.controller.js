d7eb346142ce18d12f5717aefaf04ec6e7573e63
Linh Hoang <linh.hoang@htklabs.com>
2016-10-03 09:45:05 +0700

    first commit dev

---
+++ client/components/leftbar/leftbar.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('chosidaApp')
+  .controller('LeftbarController',function ($http){
+    var self = this;
+     $http.get('components/utils/menu.json').then(function(response) {
+      self.menu = response.data;
+      console.log(self.menu);
+    });
+});
+
+angular.module('chosidaApp')
+.component('leftShop', {
+  templateUrl: 'components/leftbar/leftbar.html',
+  controller: 'LeftbarController',
+  controllerAs :'leftbar'
+});
