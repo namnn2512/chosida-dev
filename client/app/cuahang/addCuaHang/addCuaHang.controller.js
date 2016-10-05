b689490ff0d63fe00cb20b033a4fdb2e0f9a4dc7
nam nguyen <nam.k15cmu@gmail.com>
2016-10-05 23:42:58 +0700

    update add cua hang

--- client/app/cuahang/addCuaHang/addCuaHang.controller.js
+++ client/app/cuahang/addCuaHang/addCuaHang.controller.js
@@ -1,24 +1,38 @@
 'use strict';
 
 angular.module('chosidaApp')
-    .controller('AddStoreCtrl', function ($scope, $http, ManageStore) {
+    .controller('AddStoreCtrl', function($scope, $http, ManageStore, Auth) {
+        $scope.store = {};
         $scope.errors = {};
         $scope.listOfCategories = {};
-
-        $http.get('/menu').then(function (response) {
+        $scope.cities = {};
+        $http.get('/menu').then(function(response) {
             $scope.listOfCategories = response.data;
-        },function (response) {
-                //error callback
-            });
+        }, function(response) {
+            //error callback
+        });
+        $http.get('/city').then(function(response) {
+            $scope.cities = response.data;
+        }, function(response) {
+            //error callback
+        });
 
-        $scope.addNewStore = function (form) {
+
+        $scope.addNewStore = function(form) {
             $scope.submitted = true;
             if (form.$valid) {
-                ManageStore.createStore(form)
-                    .then(function () {
+                ManageStore.createStore({
+                    name: $scope.store.name,
+                    info: $scope.store.info,
+                    address: $scope.store.address,
+                    cityId: $scope.store.cityId,
+                    imgUrl: $scope.store.imgUrl,
+                    categoryId: $scope.store.categoryId,
+                })
+                    .then(function() {
                         $scope.message = 'Thêm cửa hàng thành công.';
                     })
-                    .catch(function () {
+                    .catch(function() {
                         form.password.$setValidity('mongoose', false);
                         $scope.errors.other = 'Incorrect password';
                         $scope.message = '';
