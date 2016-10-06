5a2ea8c7231f5ee7a2fc19dde2bb5b22277a8568
nam nguyen <nam.k15cmu@gmail.com>
2016-10-06 16:11:56 +0700

    Update addCuaHang.controller.js

--- client/app/cuahang/addCuaHang/addCuaHang.controller.js
+++ client/app/cuahang/addCuaHang/addCuaHang.controller.js
@@ -45,13 +45,12 @@ angular.module('chosidaApp')
                 UploadFile.upload($scope.file) //call upload function
                 .then(function (resp) { //upload function returns a promise
                     if (resp.data.error_code === 0) { //validate success
-                        $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                        console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
                     } else {
-                        $window.alert('an error occured');
+                        console.log('an error occured');
                     }
                 }, function (resp) { //catch error
                     console.log('Error status: ' + resp.status);
-                    $window.alert('Error status: ' + resp.status);
                 }, function (evt) {
                     console.log(evt);
                     var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
