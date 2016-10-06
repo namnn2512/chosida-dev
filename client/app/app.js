344e35aa1f88ca1c5f0b2763faa5f9b4df03bc89
nam nguyen <nam.k15cmu@gmail.com>
2016-10-06 15:37:09 +0700

    Update app.js

--- client/app/app.js
+++ client/app/app.js
@@ -6,7 +6,8 @@ angular.module('chosidaApp', [
   'ngSanitize',
   'btford.socket-io',
   'ui.router',
-  'ui.bootstrap'
+  'ui.bootstrap',
+  'ngFileUpload'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
