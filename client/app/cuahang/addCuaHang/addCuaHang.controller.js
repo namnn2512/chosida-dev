de8cdb7b973634c29dfa0d58652a236e265582a0
nam nguyen <nam.k15cmu@gmail.com>
2016-10-06 14:57:01 +0700

    Update addCuaHang.controller.js

--- client/app/cuahang/addCuaHang/addCuaHang.controller.js
+++ client/app/cuahang/addCuaHang/addCuaHang.controller.js
@@ -39,4 +39,25 @@ angular.module('chosidaApp')
                     });
             }
         };
+  
+        $scope.uploadImage = function () {
+            if ($scope.upload_form.file.$valid && $scope.file) { //check if from is valid
+                UploadFile.upload($scope.file) //call upload function
+                .then(function (resp) { //upload function returns a promise
+                    if (resp.data.error_code === 0) { //validate success
+                        $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
+                    } else {
+                        $window.alert('an error occured');
+                    }
+                }, function (resp) { //catch error
+                    console.log('Error status: ' + resp.status);
+                    $window.alert('Error status: ' + resp.status);
+                }, function (evt) {
+                    console.log(evt);
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+                    $scope.progress = 'progress: ' + progressPercentage + '% '; // capture upload progress
+                });
+            }
+        };
     });
