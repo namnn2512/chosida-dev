efeb80db22087962bc86a6bd1f64023b3c97be87
nam nguyen <nam.k15cmu@gmail.com>
2016-10-15 17:09:42 +0700

    Update leftbar.controller.js

--- client/components/leftbar/leftbar.controller.js
+++ client/components/leftbar/leftbar.controller.js
@@ -1,19 +1,55 @@
 'use strict';
 
 angular.module('chosidaApp')
-  .controller('LeftbarController',function ($http){
-    var self = this;
-    $http.get('/menu').then(function(response) {
-      self.menu = response.data;
-    },
-    function(response){
-        //error callback
+    .service('MenuService', function($cacheFactory, $http, $q) {
+
+        var cache = $cacheFactory('dataCache');
+
+        function success(data, status, headers, config) {
+            // some success thing
+        }
+
+        function error(data, stats, headers, config) {
+            // some error thing
+        }
+
+        this.getMenu = function() {
+            var deferred = $q.defer();
+            var cacheId = 'menuCategory';
+            var cachedData = cache.get(cacheId);
+            // Return the data if we already have it
+            if (cachedData) {
+                //success(cachedData);
+                deferred.resolve(cachedData);
+                return deferred.promise;
+            }
+
+
+            $http.get('/menu').then(function(response) {
+                //self.menu = response.data;
+                cache.put(cacheId, response.data);
+                deferred.resolve(response.data);
+
+                //success(data, status, headers, configs);
+            },
+                function(response) {
+                    //error callback
+                });
+            return deferred.promise;
+        };
+
+    })
+
+    .controller('LeftbarController', function(MenuService) {
+        var self = this;
+        MenuService.getMenu().then(function(menu) {
+            self.menu = menu;
+        });
     });
-});
 
 angular.module('chosidaApp')
-.component('leftShop', {
-  templateUrl: 'components/leftbar/leftbar.html',
-  controller: 'LeftbarController',
-  controllerAs :'leftbar'
-});
+    .component('leftShop', {
+        templateUrl: 'components/leftbar/leftbar.html',
+        controller: 'LeftbarController',
+        controllerAs: 'leftbar'
+    });
